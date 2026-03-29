b55ae477f97489dc23a1e956f8fcf9e22e36ef79
Mihai Gurduza <58224453+m33ga@users.noreply.github.com>
2026-03-29 19:40:07 +0300

    feat: support plaintext responses (#26)

--- src/go2web/http/parsers/__init__.py
+++ src/go2web/http/parsers/__init__.py
@@ -1,6 +1,4 @@
 from go2web.http.parsers.abstract_parser import Parser
-from go2web.http.parsers.html_parser import HTMLParser
-from go2web.http.parsers.json_parser import JSONParser
 from go2web.http.parsers.parser_manager import ParserManager
 
-__all__ = ["Parser", "HTMLParser", "JSONParser", "ParserManager"]
+__all__ = ["Parser", "ParserManager"]
